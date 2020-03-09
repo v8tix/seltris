bbf3b95840fec66d0dcf5aa60593dd5a990e17de
Marco Almeida <marcoalmeida@v8tix.com>
2020-03-09 18:54:17 -0500

    Updated to laskaris:0.0.2.v20200309.d0929cc.

--- configuration/const.sh
+++ configuration/const.sh
@@ -1,4 +1,4 @@
-TAG="0.0.1.v20200308"
+TAG="0.0.2.v20200309"
 IMAGE="seltris"
 IMAGE_TAG=""${IMAGE}":"${TAG}""
 HOST_PORT=8080
@@ -6,6 +6,6 @@ CNTR_PORT=8080
 HOST_MICROSERVICE_DIR="/home/v8tix/Microservices/social/"
 CNTR_MICROSERVICE_DIR="/home/v8tix/microservice"
 JAVA_IO_DIR="/home/v8tix/temp/"
-FATJAR_NAME="social-1.1.4.v20190906.jar"
+FATJAR_NAME="social-1.0.0.v20200309.jar"
 FATJAR=""${CNTR_MICROSERVICE_DIR}"/fatjar/"${FATJAR_NAME}""
 CONFIG=""${CNTR_MICROSERVICE_DIR}"/config/"
